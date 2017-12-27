cc3d3ed7901ba77e4a9644ba4b3b101644577e80
张玉昌 <990397637@qq.com>
2017-12-27 19:22:23 +0800

    代码块

---
+++ YJTest/YJTest/YJCodeBlockViewController.h
@@ -0,0 +1,13 @@
+//
+//  YJCodeBlockViewController.h
+//  YJTest
+//
+//  Created by zyc on 2017/12/27.
+//  Copyright © 2017年 zyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJCodeBlockViewController : UIViewController
+
+@end
---
+++ YJTest/YJTest/YJCodeBlockViewController.m
@@ -0,0 +1,60 @@
+//
+//  YJCodeBlockViewController.m
+//  YJTest
+//
+//  Created by zyc on 2017/12/27.
+//  Copyright © 2017年 zyc. All rights reserved.
+//
+
+#import "YJCodeBlockViewController.h"
+
+@interface YJCodeBlockViewController ()
+
+@end
+
+@implementation YJCodeBlockViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    /*
+     1.圆角
+     
+     2.block属性 和方法
+     
+     3.c 方法
+     
+     4.子线程切换主线程
+     
+     5.tableView 代理
+     
+     6.collectionView 代理
+     
+     7.单例创建
+     
+     8.文件写入，读取
+     
+     9.模拟器路径
+     
+     */
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

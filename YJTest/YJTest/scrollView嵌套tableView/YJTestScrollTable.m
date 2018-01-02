f20f4a39cde80db3f60a96d25be7a0ae8ff4d810
张玉昌 <990397637@qq.com>
2018-01-02 18:51:51 +0800

    test

--- YJTest/YJTest/scrollView嵌套tableView/YJTestScrollTable.m
+++ YJTest/YJTest/scrollView嵌套tableView/YJTestScrollTable.m
@@ -7,6 +7,8 @@
 //
 
 #import "YJTestScrollTable.h"
+#import "YJTestScrollScrollView.h"
+
 @interface YJTestScrollTable()<UITableViewDelegate, UITableViewDataSource>
 
 @end
@@ -52,13 +54,54 @@
 }
 
 
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+// tableView 不走这里
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"%@",touches);
+    NSLog(@"%@",event);
 }
-*/
+
+#pragma mark - UIGestureRecognizerDelegate
+
+//开始进行手势识别时调用的方法，返回NO则结束识别，不再触发手势，用处：可以在控件指定的位置使用手势识别
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    return YES;
+}
+
+//是否支持多手势触发，返回YES，则可以多个手势一起触发方法，返回NO则为互斥. 默认为NO
+//是否允许多个手势识别器共同识别，一个控件的手势识别后是否阻断手势识别继续向下传播，默认返回NO；如果为YES，响应者链上层对象触发手势识别后，如果下层对象也添加了手势并成功识别也会继续执行，否则上层对象识别后则不再继续传播
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    
+    //    if ([otherGestureRecognizer.view isKindOfClass:NSClassFromString(@"UICollectionView")]) {
+    //
+    //        if ([otherGestureRecognizer.view.className isEqualToString:@"JHHomeCollectionView"]) {
+    //            return YES;
+    //        }
+    //    }
+    
+    UIView *view1 = gestureRecognizer.view;
+    CGPoint point1_in_view1 = [gestureRecognizer locationInView:view1];
+    
+    
+    UIView *view2 = otherGestureRecognizer.view;
+    CGPoint point2_in_view2 = [otherGestureRecognizer locationInView:view2];
+    
+    
+    CGPoint point1_in_view2 = [gestureRecognizer locationInView:view2];
+    CGPoint point2_in_view1 = [gestureRecognizer locationInView:view2];
+    
+    
+    if([view1 isKindOfClass:[self class]]){
+        if ([view2 isKindOfClass:[YJTestScrollScrollView class]]) {
+//            return YES;
+        }
+    }
+    
+    return YES;
+//    return NO;
+
+}
 
 @end
--- YJTest/YJTest/scrollView嵌套tableView/YJTestScrollViewController.m
+++ YJTest/YJTest/scrollView嵌套tableView/YJTestScrollViewController.m
@@ -27,37 +27,56 @@
     
     
     [self addSubViews];
+    
 }
-
+- (void)info
+{
+    //shouldRecognizeSimultaneouslyWithGestureRecognizer 代理中
+    // 默认为NO。子视图滑动不影响父视图，两个不影响
+    // 子视图设为YES时，子视图滑动，父视图会跟着滑
+    //
+    
+}
 -(void)addSubViews
 {
-    _scrollView = [[YJTestScrollScrollView alloc] initWithFrame:self.view.bounds];
+    _scrollView = [[YJTestScrollScrollView alloc] initWithFrame:CGRectMake(0, 64, ScreenWidth, ScreenHight - 64)];
     _scrollView.delegate = self;
     
-    _scrollView.backgroundColor = [UIColor greenColor];
+    _scrollView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"yj_test_img.png"]];
     [self.view addSubview:_scrollView];
     
     
-    UIView *recmendView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 300)];
     
+    UIView *recmendView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, 300 - 50, 800)];
+    recmendView.backgroundColor = [UIColor redColor];
+                                   
+    [self.scrollView addSubview:recmendView];
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, 800 + 64);
     
-    recmendView.backgroundColor = [UIColor purpleColor];
-    
-    [_scrollView addSubview:recmendView];
-    
-    
-    
-    
-    _tableView = [[YJTestScrollTable alloc] initWithFrame:CGRectMake(0, 300, self.view.frame.size.width, self.view.bounds.size.height - 44)];
-    
+//
+//
+//    recmendView.backgroundColor = [UIColor purpleColor];
+//
+//    [_scrollView addSubview:recmendView];
+//
+//
+//
+//
+    _tableView = [[YJTestScrollTable alloc] initWithFrame:CGRectMake(0, 20, recmendView.width, recmendView.height - 20)];
+//
     [self.scrollView addSubview:_tableView];
-    
-    self.scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, _tableView.frame.origin.y +_tableView.frame.size.height);
+//
+    // contentSize 大于scrollView的size才会滚动
+//    self.scrollView.contentSize = CGSizeMake(self.view.bounds.size.width, _tableView.frame.origin.y +_tableView.frame.size.height);
     
     
     [_tableView reloadData];
     
-
+    // 1
+//    _scrollView.scrollEnabled = NO;
+//    _tableView.scrollEnabled =
+    
+    
     
 }
 
@@ -68,14 +87,14 @@
     
     CGFloat y = _tableView.frame.origin.y;
     
-//    if(offsetY >= y){
-//        
-//        _tableView.scrollEnabled = YES;
-//        
-//    }else{
-//        _tableView.scrollEnabled = NO;
-//
-//     }
+    if(offsetY >= y){
+        
+        _tableView.scrollEnabled = YES;
+        
+    }else{
+        _tableView.scrollEnabled = NO;
+
+     }
     
     
     
@@ -84,6 +103,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 /*
 #pragma mark - Navigation

2a561cc9da9881f9568013ca902519c7f12d82b5
zyc_hdj <990397637@qq.com>
2018-05-15 20:57:19 +0800

    视频剪辑 尝试

---
+++ YJTest/YJTest/YJVideoCutController.h
@@ -0,0 +1,17 @@
+//
+//  YJVideoCutController.h
+//  YJTest
+//
+//  Created by zyc_hdj on 2018/5/14.
+//  Copyright © 2018年 zyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJVideoCutController : UIViewController
+
+// 1.用系统 UIVideoEditorController 来实现
+// 2 优先 可以用腾讯云SDK 3.9及之前的版本，（之后的版本需要lisence收费）※
+// 3. github开源库 ICGVideoTrimmer
+
+@end
